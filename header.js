ca15599a27b0faf1f200d0a29037435e59b14224
alexandr <sasharom1998@mail.ru>
2019-05-25 20:02:27 +0300

    Начало преобразования под react

---
+++ header.js
@@ -0,0 +1,10 @@
+class SimpleHeader extends React.Component {
+    render() {
+        return <div class = "header" > < span > Социальные сети < /span></div > ;
+    }
+}
+
+ReactDOM.render( <
+    SimpleHeader > < /SimpleHeader>,
+    document.getElementById("app");
+)
